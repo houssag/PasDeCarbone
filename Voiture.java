0a656929f2278812de191eef172985a46182c82a
MartinProbestau <60342920+MartinProbestau@users.noreply.github.com>
2020-01-30 12:08:16 +0100

    Constructeur modifié

--- Voiture.java
+++ Voiture.java
@@ -1,18 +1,18 @@
 public class Voiture extends Equipement {
-  static int DIESEL = 2640;// Variable d'émission CO2 pour un litre de diesel consommé
-  static int ESSENCE = 2392;// Variable d'émission CO2 pour un litre d'essence consommé
-  static int LPG = 1665;// Variable d'émission CO2 pour un litre de LPG consommé
-  static int CNG = 1819;// Variable d'émission CO2 pour un Nm3 de CNG consommé
+  static int DIESEL = 2640;// Variable d'Ã©mission CO2 pour un litre de diesel consommÃ©
+  static int ESSENCE = 2392;// Variable d'Ã©mission CO2 pour un litre d'essence consommÃ©
+  static int LPG = 1665;// Variable d'Ã©mission CO2 pour un litre de LPG consommÃ©
+  static int CNG = 1819;// Variable d'Ã©mission CO2 pour un Nm3 de CNG consommÃ©
 
-  // private String modele;//le modèle de la voiture
-  public enum TypeCarburant { // les différents types de carburant
+  // private String modele;//le modÃ¨le de la voiture
+  public enum TypeCarburant { // les diffÃ©rents types de carburant
     Diesel, Essence, LPG, CNG
   }
 
   private TypeCarburant carburant;
-  private float litresParKm; // les litres (ou Nm3 dans le cas du CNG) émis en un km parcouru
+  private float litresParKm; // les litres (ou Nm3 dans le cas du CNG) Ã©mis en un km parcouru
 
-  Voiture(String n, TypeCarburant c, float lpk, int np) {
+  Voiture(String n, TypeCarburant c, float lpk) {
     super(n);
     // modele=m;
     carburant = c;
@@ -20,8 +20,8 @@ public class Voiture extends Equipement {
   }
   
   /**
-   * Renvoie le taux calculé en fonction du carburant et de la consommation en litres pas km.
-   * @return Le taux d'émission CO2 de la voiture
+   * Renvoie le taux calculÃ© en fonction du carburant et de la consommation en litres pas km.
+   * @return Le taux d'Ã©mission CO2 de la voiture
    */
   public float calculerTaux() {
     float total;
@@ -41,80 +41,80 @@ public class Voiture extends Equipement {
   }
 
   /**
-   * Renvoie la valeur en g de CO2 émis par un litre de Diesel.
-   * @return La valeur en g de CO2 émis par un litre de Diesel
+   * Renvoie la valeur en g de CO2 Ã©mis par un litre de Diesel.
+   * @return La valeur en g de CO2 Ã©mis par un litre de Diesel
    */
   public static int getDiesel() {
     return DIESEL;
   }
 
   /**
-   * Modifie la valeur en g de CO2 émis par un litre de diesel.
-   * @param diesel La valeur en g de CO2 émis par un litre de diesel
+   * Modifie la valeur en g de CO2 Ã©mis par un litre de diesel.
+   * @param diesel La valeur en g de CO2 Ã©mis par un litre de diesel
    */
   public static void setDiesel(int diesel) {
     DIESEL = diesel;
   }
 
   /**
-   * Renvoie la valeur en g de CO2 émis par un litre d'essence.
-   * @return La valeur en g de CO2 émis par un litre d'essence
+   * Renvoie la valeur en g de CO2 Ã©mis par un litre d'essence.
+   * @return La valeur en g de CO2 Ã©mis par un litre d'essence
    */
   public static int getEssence() {
     return ESSENCE;
   }
 
   /**
-   * Modifie la valeur en g de CO2 émis par un litre d'essence.
-   * @param essence La valeur en g de CO2 émis par un litre d'essence
+   * Modifie la valeur en g de CO2 Ã©mis par un litre d'essence.
+   * @param essence La valeur en g de CO2 Ã©mis par un litre d'essence
    */
   public static void setEssence(int essence) {
     ESSENCE = essence;
   }
 
   /**
-   * Renvoie la valeur en g de CO2 émis par un litre de LPG.
-   * @return La valeur en g de CO2 émis par un litre de LPG
+   * Renvoie la valeur en g de CO2 Ã©mis par un litre de LPG.
+   * @return La valeur en g de CO2 Ã©mis par un litre de LPG
    */
   public static int getLpg() {
     return LPG;
   }
 
   /**
-   * Modifie la valeur en g de CO2 émis par un litre de LPG.
-   * @param lpg La valeur en g de CO2 émis par un litre de LPG
+   * Modifie la valeur en g de CO2 Ã©mis par un litre de LPG.
+   * @param lpg La valeur en g de CO2 Ã©mis par un litre de LPG
    */
   public static void setLpg(int lpg) {
     LPG = lpg;
   }
 
   /**
-   * Renvoie la valeur en g de CO2 émis par un litre de CNG.
-   * @return La valeur en g de CO2 émis par un litre de CNG
+   * Renvoie la valeur en g de CO2 Ã©mis par un litre de CNG.
+   * @return La valeur en g de CO2 Ã©mis par un litre de CNG
    */
   public static int getCng() {
     return CNG;
   }
 
   /**
-   * Modifie la valeur en g de CO2 émis par un Nm3 de CNG.
-   * @param cng La valeur en g de CO2 émis par un Nm3 de CNG
+   * Modifie la valeur en g de CO2 Ã©mis par un Nm3 de CNG.
+   * @param cng La valeur en g de CO2 Ã©mis par un Nm3 de CNG
    */
   public static void setCng(int cng) {
     CNG = cng;
   }
 
   /**
-   * Renvoie le type de carburant utilisé.
-   * @return le type de carburant utilisé
+   * Renvoie le type de carburant utilisÃ©.
+   * @return le type de carburant utilisÃ©
    */
   public TypeCarburant getCarburant() {
     return carburant;
   }
 
   /**
-   * Modifie le type de carburant utilisé.
-   * @param c Type de carburant utilisé désormais.
+   * Modifie le type de carburant utilisÃ©.
+   * @param c Type de carburant utilisÃ© dÃ©sormais.
    */
   public void setCarburant(TypeCarburant c) {
     carburant = c;
