96abaa7a9b53df34aba194804da339848858bd55
ClemD29 <59454673+ClemD29@users.noreply.github.com>
2020-01-30 11:37:33 +0100

    Main CheckStyle

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
-	public static void main(String[] args) {
-		ListeUtilisateur liste = new ListeUtilisateur();
-		Particulier P1 = new Particulier("P1","mdp","Michel","Leblanc","","michel@mail.fr");
-		Particulier P2 = new Particulier("P2","mdp2","Roger","Lenoir","","Roger@mail.fr");
-		liste.ajouterUtilisateur(P1);
-		liste.ajouterUtilisateur(P2);
-		System.out.println(P1.getPseudo());
-		System.out.println(liste.toStringListA());
-	}
+  /** Classe de test. **/
+  public static void main(String[] args) {
+    ListeUtilisateur liste = new ListeUtilisateur();
+    Particulier p1 = new Particulier("P1", "mdp", "Michel", "Leblanc", "", "michel@mail.fr");
+    Particulier p2 = new Particulier("P2", "mdp2", "Roger", "Lenoir", "", "Roger@mail.fr");
+    liste.ajouterUtilisateur(p1);
+    liste.ajouterUtilisateur(p2);
+    System.out.println(p1.getPseudo());
+    System.out.println(liste.toStringListA());
+  }
 }
