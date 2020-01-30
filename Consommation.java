f39b1683a68084d0338a1dfcc40e8b8b8d0c7dbf
DocR3D <yonnet.maxime@laposte.net>
2020-01-30 11:58:27 +0100

    Ajout de la liste de distance

--- Consommation.java
+++ Consommation.java
@@ -4,27 +4,41 @@ import java.util.Date;
 public class Consommation {
   private Utilisateur unUtilisateur;
   private ArrayList<Equipement> listeEquipement;
+  private ArrayList<Integer> listeDistance;
   private Date mois;
   private float empreinte;
 
 
-  /** Constructor de la classe consommation **/
-  Consommation(Utilisateur unUtilisateur, Equipement unEquipement, Date mois) {
+  /** Constructor consommation
+   * 
+   * @param unUtilisateur correspond à l'utilisateur pour lequel la consommation existe
+   * @param unEquipement correspond à au premier équipement que l'utilisateur a utilisé
+   * @param distance
+   * @param mois
+   * 
+   */
+  Consommation(Utilisateur unUtilisateur, Equipement unEquipement, int distance, Date mois) {
     this.unUtilisateur = unUtilisateur;
     this.listeEquipement = new ArrayList<Equipement>();
+    this.listeDistance = new ArrayList<Integer>();
     this.listeEquipement.add(unEquipement);
+    this.listeDistance.add(distance);
     this.mois = mois;
-    ListeConsommation.AjouterConsommation(this);
+    ListeConsommation.ajouterConsommation(this);
   }
 
-  float calculerConsommationVehicule(int distance) {
+  /** Description calculerConsommation.
+   * 
+   * @return renvoie sous forme d'entier la valeur de la consommation d'un mois pour un utilisateur
+   */
+  float calculerConsommation() {
     float taux = 0;
-    for (Equipement unAutreEquipement : this.listeEquipement) {
-      taux = unAutreEquipement.calculerTaux();
+    for (int i = 0; i < this.listeEquipement.size(); i++) {
+      taux = taux + this.listeEquipement.get(i).calculerTaux() * this.listeDistance.get(i);
     }
-
     return taux;
   }
+
 
   /*
    * boolean equals(Consommation uneConsommation) { if(uneConsommation == null) return false;
@@ -41,12 +55,38 @@ public class Consommation {
     this.unUtilisateur = unUtilisateur;
   }
 
-  public ArrayList<Equipement> getEquipement() {
-    return listeEquipement;
+  /** Description ajotuerUnEquipementEtUneDistance.
+   * 
+   * @param unEquipement correspond à l'équipement à ajouter à la liste d'équipement
+   * @param distance la distance à ajotuer à la liste des distances
+   */
+  public void ajouterUnEquipementEtUneDistance(Equipement unEquipement, int distance) {
+    this.listeEquipement.add(unEquipement);
+    this.listeDistance.add(distance);
+  }
+  
+  /** Description enleverUnEquipementEtUneDistance.
+   * 
+   * @param unEquipement correspond à l'équipement à supprimer
+   */
+  public void enleverUnEquipementEtUneDistance(Equipement unEquipement) {
+    int i = this.listeEquipement.indexOf(unEquipement);
+    this.listeEquipement.remove(unEquipement);
+    this.listeDistance.remove(i);
   }
 
-  public void addUnEquipement(Equipement unEquipement) {
-    this.listeEquipement.add(unEquipement);
+  /** Description getDistance.
+   * 
+   * @param unEquipement correspond à l'équipement pour lequel récuperer la distance.
+   * @return Renvoie la distance parcouru avec l'équipement.
+   */
+  public Integer getDistance(Equipement unEquipement) {
+    for (int i = 0; i < this.listeEquipement.size(); i++) {
+      if (this.listeEquipement.get(i).equals(unEquipement)) {
+        return this.listeDistance.get(i);
+      }
+    }
+    return 0;
   }
 
   public Date getMois() {
