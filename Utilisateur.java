39009262c806e8280ac20be1555adb102a0d9483
houssag <59451548+houssag@users.noreply.github.com>
2020-01-30 11:31:10 +0100

    Check style Utilisateur

--- Utilisateur.java
+++ Utilisateur.java
@@ -1,111 +1,129 @@
 public abstract class Utilisateur {
-	private String pseudo;
-	private String mdp;
-	private String prenom;
-	private String nom;
-	private String adresse;
-	private String mail;
-	
-	/** Constructeur de la classe Utilisateur **/
-	
-	Utilisateur(String p, String m, String pre, String n, String ad,String ma){
-		if(p.isEmpty() != true && m.isEmpty() != true && ma.isEmpty() != true) { // On intialise le pseudo et le mot de passe si ils ne sont pas vides
-			this.pseudo = p;
-			this.mdp = m;
-			this.mail = ma;
-			
-			if(pre.isEmpty() != true) {	
-				if(contientChiffre(pre) == false) { // On initialise le prénom si il n'est pas vide et si il ne contient pas de chiffre
-					this.prenom = pre;
-				}
-			}
-			if(n.isEmpty() != true) { // On initialise le nom si il n'est pas vide et si il ne contient pas de chiffre
-				if(contientChiffre(n) == false) {
-					this.nom = n;
-				}
-			}
-			if(ad.isEmpty() != true) { // On intialise l'adresse si elle n'est pas vide (elle peut contenir des chiffres)
-				this.adresse = ad;
-			}
-		}
+  private String pseudo;
+  private String mdp;
+  private String prenom;
+  private String nom;
+  private String adresse;
+  private String mail;
 
-	}
+  /** Constructeur de la classe Utilisateur. **/
 
-	/** Getters et Setters **/
-	
-	public String getPseudo() {
-		return pseudo;
-	}
+  Utilisateur(String p, String m, String pre, String n, String ad, String ma) {
+    
+    // On intialise le pseudo et le mot de passe si ils ne sont pas vides
+    if (p.isEmpty() != true && m.isEmpty() != true && ma.isEmpty() != true) { 
 
-	public void setPseudo(String pseudo) {
-		if(pseudo.isEmpty() != true) {
-			this.pseudo = pseudo;	
-		}
-	}
+      this.pseudo = p;
+      this.mdp = m;
+      this.mail = ma;
 
-	public String getMdp() {
-		return mdp;
-	}
+      if (pre.isEmpty() != true) {
+        // On initialise le prénom si il n'est pas vide et si il ne contient pas de chiffre
+        if (contientChiffre(pre) == false) { 
+          this.prenom = pre;
+        }
+      }
+      // On initialise le nom si il n'est pas vide et si il ne contient pas de chiffre
+      if (n.isEmpty() != true) { 
+        if (contientChiffre(n) == false) {
+          this.nom = n;
+        }
+      }
+      // On intialise l'adresse si elle n'est pas vide (elle peut contenir des chiffres)
+      if (ad.isEmpty() != true) { 
+        this.adresse = ad;
+      }
+    }
 
-	public void setMdp(String mdp) {
-		if (mdp.isEmpty() != true) {
-			this.mdp = mdp;
-		}
-	}
-	
-	public String getMail() {
-		return mail;
-	}
-	
-	public void setMail(String mail) {
-		if(mail.isEmpty() != true) {
-			this.mail = mail;
-		}
-	}
+  }
 
-	public String getPrenom() {
-		return prenom;
-	}
+  /** Getters et Setters. **/
 
-	public void setPrenom(String prenom) {
-		if( prenom.isEmpty() != true) {
-			if(contientChiffre(prenom) == false) {
-				this.prenom = prenom;
-			}
-		}
-	}
+  /** Récupère la valeur du pseudo. **/
+  public String getPseudo() {
+    return pseudo;
+  }
 
-	public String getNom() {
-		return nom;
-	}
+  /** Change la valeur du pseudo. **/
+  public void setPseudo(String pseudo) {
+    if (pseudo.isEmpty() != true) {
+      this.pseudo = pseudo;
+    }
+  }
 
-	public void setNom(String nom) {
-		if( nom.isEmpty() != true) {
-			if(contientChiffre(nom) == false) {
-				this.nom =nom;
-			}
-		}
-	}
+  /** Récupère la valeur du mot de passe. **/
+  public String getMdp() {
+    return mdp;
+  }
 
-	public String getAdresse() {
-		return adresse;
-	}
+  /** Change la valeur du mot de passe. **/
+  public void setMdp(String mdp) {
+    if (mdp.isEmpty() != true) {
+      this.mdp = mdp;
+    }
+  }
 
-	public void setAdresse(String adresse) {
-		if(adresse.isEmpty() != true) {
-			this.adresse = adresse;			
-		}
-	}
+  /** Récupère la valeur du mail. **/
+  public String getMail() {
+    return mail;
+  }
 
-	/** Méthode pour vérifier qu'une chaine de caractères ne contient pas de chiffres **/
-	
-	public boolean contientChiffre(String s) {
-		boolean ok = false;
-		for(int i=0;i<s.length();i++) {
-			if(Character.isDigit(s.charAt(i)) == true) {
-				ok = true;
-			}
-		}
-		return ok;
-	}
+  /** Change la valeur du mail. **/
+  public void setMail(String mail) {
+    if (mail.isEmpty() != true) {
+      this.mail = mail;
+    }
+  }
+
+  /** Récupère la valeur du prénom. **/
+  public String getPrenom() {
+    return prenom;
+  }
+
+  /** Change la valeur du prénom. **/
+  public void setPrenom(String prenom) {
+    if (prenom.isEmpty() != true) {
+      if (contientChiffre(prenom) == false) {
+        this.prenom = prenom;
+      }
+    }
+  }
+
+  /** Récupère la valeur du nom. **/
+  public String getNom() {
+    return nom;
+  }
+
+  /** Change la valeur du nom. **/
+  public void setNom(String nom) {
+    if (nom.isEmpty() != true) {
+      if (contientChiffre(nom) == false) {
+        this.nom = nom;
+      }
+    }
+  }
+
+  /** Récupère la valeur de l'adresse. **/
+  public String getAdresse() {
+    return adresse;
+  }
+
+  /** Change la valeur de l'adresse. **/
+  public void setAdresse(String adresse) {
+    if (adresse.isEmpty() != true) {
+      this.adresse = adresse;
+    }
+  }
+
+  /** Méthode pour vérifier qu'une chaine de caractères ne contient pas de chiffres. **/
+
+  public boolean contientChiffre(String s) {
+    boolean ok = false;
+    for (int i = 0; i < s.length(); i++) {
+      if (Character.isDigit(s.charAt(i)) == true) {
+        ok = true;
+      }
+    }
+    return ok;
+  }
 }
