02c7c2e4bd7fe03a5e154a753a2bf581345e7822
MartinProbestau <60342920+MartinProbestau@users.noreply.github.com>
2020-01-31 11:17:57 +0100

    Super chauffage javadocé

--- Chauffage.java
+++ Chauffage.java
@@ -3,13 +3,16 @@ public class Chauffage extends Equipement {
   public enum TypeChauffage {
     FioulDomestique, FioulLourd, GazNaturel, GazPropaneOuButane, Charbon, Bois, Electricité
   }
+  // les types de chauffage possibles
 
-  TypeChauffage type;
+  TypeChauffage type; // Le type du chauffage
   static int[] consoChauffagekwh = {300, 320, 234, 274, 384, 13, 180};
-  private int kwh;
-  private boolean appartement;
-  private boolean apres1975;
-  private int surface;
+  // les émissions de CO2 des types de chauffages en g/kWh, arrangés de façon à correspondre à
+  // l'ordre des types de chauffage.
+  private int kwh; // le nombre de kWh consommé en un mois
+  private boolean appartement; // Vrai si le bâtiment est un appartement, faux si maison.
+  private boolean apres1975; // Vrai si le bâtiment a été construit après 1975, faux sinon.
+  private int surface; // Surface chauffée en m2
   static int[][] consoChauffage = { // première colonne avant 1975, deuxième après
                                     // maison
       {6667, 5000, // fioul domestique
@@ -27,9 +30,10 @@ public class Chauffage extends Equipement {
           3551, 3551, // charbon
           198, 159, // bois
           1667, 1667}}; // électricité
-  
+
   /**
    * Constructeur de l'objet chauffage si on a la consommation en kWh.
+   * 
    * @param n nom du chauffage
    * @param t type du chauffage
    * @param k consommation en kWh pour le mois
@@ -44,6 +48,7 @@ public class Chauffage extends Equipement {
 
   /**
    * Constructeur de l'objet chauffage si on n'a pas la consommation en kWh.
+   * 
    * @param n nom du chauffage
    * @param t type du chauffage
    * @param a vrai si on chauffe un appartement, faux si maison
@@ -60,9 +65,10 @@ public class Chauffage extends Equipement {
   }
 
   /**
-   * Si kwh est différent de -1, cela veut dire que le constructeur avec la consommation en kWh a été utilisé,
-   * on procède au calcul avec le kWh, sinon on calcule en fonction de la surface, du type d'habitation et de sa date de construction.
-   * Le type de chauffage est toujours pris en compte.
+   * Si kwh est différent de -1, cela veut dire que le constructeur avec la consommation en kWh a
+   * été utilisé, on procède au calcul avec le kWh, sinon on calcule en fonction de la surface, du
+   * type d'habitation et de sa date de construction. Le type de chauffage est toujours pris en
+   * compte.
    */
   public float calculerTaux() {
     if (kwh != -1) {
@@ -72,12 +78,14 @@ public class Chauffage extends Equipement {
           + ordonnees(this.apres1975)];
     }
   }
-  
+
   /**
-   * Renvoie 1 si le booléen est vrai, cela nous aide à déterminer les coordonnées d'une donnée dans le tableau
-   * des consommations si on n'a pas les kWh.
-   * @param b Booléen, ce sera surtout les variables appartement et apres1975 de l'objet qui seront utilisées
-   * @return
+   * Renvoie 1 si le booléen est vrai, cela nous aide à déterminer les coordonnées d'une donnée dans
+   * le tableau des consommations si on n'a pas les kWh.
+   * 
+   * @param b Booléen, ce sera surtout les variables appartement et apres1975 de l'objet qui seront
+   *        utilisées
+   * @return 0 ou 1
    */
   public int ordonnees(boolean b) {
     if (b) {
@@ -85,6 +93,135 @@ public class Chauffage extends Equipement {
     } else {
       return 0;
     }
+  }
+
+  /**
+   * Renvoie le type de chauffage utilisé.
+   * 
+   * @return le type de chauffage utilisé
+   */
+  public TypeChauffage getType() {
+    return type;
+  }
+
+  /**
+   * Modifie le type de chauffage utilisé.
+   * 
+   * @param le nouveau type de chauffage utilisé
+   */
+  public void setType(TypeChauffage type) {
+    this.type = type;
+  }
+  
+  /**
+   * Renvoie une consommation de CO2 en g/kWh.
+   * @param t le type de chauffage dont on veut la consommation de CO2 en g/kWh
+   * @return la consommation de CO2 en g/kWh
+   */
+  public static int getConsoChauffagekwh(TypeChauffage t) {
+    return consoChauffagekwh[t.ordinal()];
+  }
+  
+  /**
+   * Modifie la consommation en g/kWh d'un certain type de chauffage.
+   * @param t le type de chauffage dont on veut modifier la consommation de CO2 en g/kWh
+   * @param c la nouvelle consommation en g/kWh
+   */
+  public static void setConsoChauffagekwh(TypeChauffage t,int c) {
+    consoChauffagekwh[t.ordinal()] = c;
+  }
+
+  /**
+   * Renvoie la consommation en kWh.
+   * 
+   * @return la consommation en kWh
+   */
+  public int getKwh() {
+    return kwh;
+  }
+
+  /**
+   * Modifie la consommation en kWh.
+   * 
+   * @param la nouvelle consommation en kWh
+   */
+  public void setKwh(int kwh) {
+    this.kwh = kwh;
+  }
+
+  /**
+   * Renvoie vrai si le bâtiment est un appartement, faux sinon.
+   * 
+   * @return true si le bâtiment est un appartement, false sinon
+   */
+  public boolean isAppartement() {
+    return appartement;
+  }
+  
+  /**
+   * Modifie le type du bâtiment (appartement ou maison).
+   * 
+   * @param appart true si le bâtiment est un appartement, false sinon
+   */
+  public void setAppartement(boolean appart) {
+    appartement = appart;
+  }
+
+  /**
+   * Renvoie vrai si le bâtiment date d'après 1975, faux sinon.
+   * 
+   * @return true si le bâtiment date d'après 1975, false sinon
+   */
+  public boolean isApres1975() {
+    return apres1975;
+  }
+
+  /**
+   * Modifie la date de construction du bâtiment (avant 1975 ou après).
+   * 
+   * @param date true si le bâtiment date d'après 1975, false sinon
+   */
+  public void setApres1975(boolean date) {
+    apres1975 = date;
+  }
+
+  /**
+   * Renvoie la surface du bâtiment chauffé en m2.
+   * 
+   * @return la surface du bâtiment chauffé en m2
+   */
+  public int getSurface() {
+    return surface;
+  }
+  /**
+   * Modifie la surface du bâtiment chauffé en m2.
+   * 
+   * @param s la nouvelle surface du bâtiment chauffé en m2
+   */
+  public void setSurface(int s) {
+    surface = s;
+  }
+
+  /**
+   * Renvoie la consommation de CO2 en g pour une surface d'1m2 dont les caractéristiques du bâtiment seront renseignées en paramètres.
+   * @param appart true si on veut la consommation pour un appartement, false si maison
+   * @param date true si on veut la consommation pour un bâtiment datant d'après 1975, false sinon
+   * @param t le type du chauffage dont on veut la consommation
+   * @return le nombre du tableau correspondant au bâtiment et type de chauffage donné
+   */
+  public int getConsoChauffage(boolean appart, boolean date, TypeChauffage t) {
+    return consoChauffage[ordonnees(appart)][t.ordinal() * 2 + ordonnees(date)];
+  }
+
+  /**
+   * Modifie la consommation de CO2 en g pour une surface d'1m2 dont les caractéristiques du bâtiment seront renseignées en paramètres.
+   * @param appart true si on veut modifier la consommation pour un appartement, false si maison
+   * @param date true si on veut modifier la consommation pour un bâtiment datant d'après 1975, false sinon
+   * @param t le type du chauffage dont on veut modifier la consommation
+   * @param c la nouvelle consommation du bâtiment
+   */
+  public void setConsoChauffage(boolean appart, boolean date, TypeChauffage t, int c) {
+    consoChauffage[ordonnees(appart)][t.ordinal() * 2 + ordonnees(date)] = c;
   }
 }
 
