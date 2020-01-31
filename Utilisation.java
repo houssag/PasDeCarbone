fa147735aa045ba10a6afdc84b8829f5ed3159f4
DocR3D <yonnet.maxime@laposte.net>
2020-01-31 08:23:06 +0100

    Update Utilisation.java

--- Utilisation.java
+++ Utilisation.java
@@ -1,10 +1,35 @@
 
 public class Utilisation {
 
+  Equipement unEquipement;
   int quantite;
+  int nbPersonne;
 
-  public Utilisation(int quantite) {
+  /**
+   * Creer une utilisation pour une utilisation par une seule personne pendant une quantité et
+   * concernant un équipement.
+   * 
+   * @param quantite désigne la quantité de l'équipement utilisé
+   * @param unEquipement désigne l'équipement utilisé
+   */
+  public Utilisation(int quantite, Equipement unEquipement) {
     this.quantite = quantite;
+    this.unEquipement = unEquipement;
+    this.nbPersonne = 1;
+  }
+
+  /**
+   * Creer une utilisation pour une utilisation par un nombre de personnes pendant une quantité et
+   * concernant un équipement.
+   * 
+   * @param quantite désigne la quantité de l'équipement utilisé
+   * @param unEquipement désigne l'équipement utilisé
+   * @param nbPersonne désigne le nombre de personne utilisant l'équipement
+   */
+  public Utilisation(int quantite, Equipement unEquipement, int nbPersonne) {
+    this.quantite = quantite;
+    this.unEquipement = unEquipement;
+    this.nbPersonne = nbPersonne;
   }
 
 
